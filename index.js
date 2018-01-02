4b6ebc37173448d07a00f424cef751f4133b299a
Ben Edgar <ben.edgar@appian.com>
2018-01-02 09:50:37 -0500

    Finished appgit stgit st!

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 // Create a component
+// Not sure if flex: 1 is necessary with this version of react-native
+// In the video tutorial the iOS device was always snapping back to the top
+// but on my android device it was working fine without the flex: 1 specification.
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View>
